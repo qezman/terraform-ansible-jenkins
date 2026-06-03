9174ebf9bb5b099761b69411e260e6bf84028627
Kazeem Jimoh <holaryinka5050@gmail.com>
2026-06-03 12:24:36 +0100

    feat: add nginx reverse proxy and SSL certificate

--- terraform/main.tf
+++ terraform/main.tf
@@ -104,6 +104,13 @@ resource "aws_security_group" "app" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   # Outbound - allow all
   egress {
